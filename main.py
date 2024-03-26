c8e6b2ee8fc9237a418844eae337c9d9fafafb1c
karina <karinavu.january@gmail.com>
2024-03-26 14:00:40 +0700

    update api_url

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from ultils.generate_word_cloud import generate_word_cloud_image
 config = dotenv_values()
 api_token = config["token"]
 repository_owner = "dear-karina"
-repository_name = "dear-karina"
+repository_name = "github_readme_updater"
 api_url = f"https://api.github.com/repos/{repository_owner}/{repository_name}/issues"
 params = {
     "state": "all",
@@ -18,10 +18,11 @@ headers = {
 response = requests.get(api_url, params=params, headers=headers)
 response.raise_for_status()
 issues = response.json()
+print(issues)
 prefix = "wordcloud|add|"
 matched_issues = [issue for issue in issues if issue["title"].startswith(prefix)]
 strings = [issue["title"][len(prefix):].strip() for issue in matched_issues]
-
+print(strings)
 issues_string = " ".join(strings)
 
 cloud_input = "live laugh love" + issues_string
