1c7db71ebb18dbde4d473ac1043d5346aaea971e
karina (khavy) <164046438+dear-karina@users.noreply.github.com>
2024-05-04 10:07:21 +0000

    added word and fact alternative database

---
+++ src/data/alternatives/beautifulWords.alternative.data.ts
@@ -0,0 +1,82 @@
+export const beautifulWords = [
+    {
+      id: 1,
+      word: "mellifluous",
+      form: "adj",
+      pronunciation: "muh-li-floo-uhs",
+      meaning: "A sound that’s pleasing and sweet to hear",
+      origin: "Latin"
+    },
+    {
+      id: 2,
+      word: "apricity",
+      form: "n",
+      pronunciation: "a-PRIS-i-tee",
+      meaning: "The warmth of the sun in winter",
+      origin: "Latin"
+    },
+    {
+      id: 3,
+      word: "rakuyou",
+      form: "n",
+      pronunciation: "ra-ku-yo",
+      meaning: "Golden fallen or shedded leaves",
+      origin: "Japanese"
+    },
+    {
+      id: 4,
+      word: "retrouvailles",
+      form: "n",
+      pronunciation: "ruh-troov-eye",
+      meaning: "The happiness of meeting again after a long time",
+      origin: "French"
+    },
+    {
+      id: 5,
+      word: "hitoritabi",
+      form: "n",
+      meaning: "Traveling alone, solitary journey",
+      origin: "Japanese"
+    },
+    {
+      id: 6,
+      word: "luftmensch",
+      form: "n",
+      pronunciation: "LOOFT-mensh",
+      meaning: "An impractical dreamer, literally an air person, someone with her head in the clouds",
+      origin: "Yiddish"
+    },
+    {
+      id: 7,
+      word: "raconteur",
+      form: "n",
+      pronunciation: "ra-kawn-tuh",
+      meaning: "A talented storyteller who’s able to spin amusing tales from everyday tales; A person who tells anecdotes skillfully and amusingly",
+      origin: "French"
+    },
+    {
+      id: 8,
+      word: "sirimiri",
+      form: "n",
+      pronunciation: "si-ri-mi-ri",
+      meaning: "A light rain; A fine drizzle; Stronger than mist but less than a shower",
+      origin: "Spanish"
+    },
+    {
+      id: 9,
+      word: "rimjhim",
+      form: "n",
+      pronunciation: "rim-jhim",
+      meaning: "The pitter-patter of a drizzle",
+      origin: "Sanskrit"
+    },
+    {
+      id: 10,
+      word: "petrichor",
+      form: "n",
+      pronunciation: "PET-ri-kuhr",
+      meaning: "The mild and pleasant smell or scent of earth associated with the first rain after a dry spell",
+      origin: "Greek"
+    }
+  ];
+  
---
+++ src/data/alternatives/marineFacts.alternative.data.ts
@@ -0,0 +1,67 @@
+export const marineFacts=[
+    {
+        id: 1,
+        title:"",
+        description:"The average depth of the ocean is 12,100 feet.",
+        keyword:["marine"]
+    },
+    {
+        id: 2,
+        title:"",
+        description:"The average depth of the ocean is 12,100 feet.",
+        keyword:["marine"]
+    },
+    {
+        id: 3,
+        title:"",
+        description:"The planet's longest mountain range is underwater and is 10 times longer than the Andes. Above water, the Andes mountain range is only about 4,300 miles long, while underwater, the Mid-Oceanic Ridge mountain range is around 40,390 miles long.",
+        keyword:["marine"]
+    },
+    {
+        id: 4,
+        title:"",
+        description:"Why are our oceans blue? Water absorbs the red light of the color spectrum, leaving the blue for us to see.",
+        keyword:["marine"]
+    },
+    {
+        id: 5,
+        title:"",
+        description:"Sound travels underwater 4.3x faster compared to air – this makes determining direction of sound near impossible for divers.",
+        keyword:["marine"]
+    },
+    {
+        id: 6,
+        title:"",
+        description:"Half of the oxygen we breathe is produced in the ocean.",
+        keyword:["marine"]
+    },
+    {
+        id: 7,
+        title:"",
+        description:"Oceans help keep Earth’s climate habitable. By moving water around the globe, the oceans help to keep places from getting too hot or too cold.",
+        keyword:["marine"]
+    },
+    {
+        id: 8,
+        title:"",
+        description:"Water at the bottom of the ocean is incredibly hot. Water coming out of hydrothermal vents in the seafloor can be up to 750º Fahrenheit. The intense pressure at these depths keeps the water from boiling.",
+        keyword:["marine"]
+    },
+    {
+        id: 9,
+        title:"",
+        description:"The words “ocean” and “sea” are often used to mean the same thing. A sea, however, is a small area of an ocean, usually with land on several sides. The Mediterranean, nestled between Africa and Europe, the Baltic in northern and central Europe, and the Caribbean between North, Central, and South America are all seas.",
+        keyword:["marine"]
+    },
+    {
+        id: 10,
+        title:"The oceans are enormous",
+        description:"Our oceans cover around 71% of the Earth’s surface, but their depth means they actually contain 99.9% of the habitable space for life on Earth. They also contain 1.35 billion cubed kilometres of water.",
+        keyword:["marine"]
+    }
+
+]
+
+//Source:
+// https://marinemadnessdotblog.wordpress.com/2020/10/14/50-mind-blowing-facts-about-the-oceans-marine-life/
+// https://wildplanetfoods.com/blogs/blog/12-interesting-ocean-facts
