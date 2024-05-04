6ec0c7563a064a78ce850f1f836ce013b81309d7
sakurasakura <82525680+sakurasakura@users.noreply.github.com>
2024-05-04 08:54:59 +0700

    Update photo.model.ts

--- model/photo.model.ts
+++ model/photo.model.ts
@@ -1,6 +1,20 @@
 // Hamin
-export type Photo = {
-  url: string;
-  description: string;
-  author: string;
-};
+
+export class Photo {
+  constructor(
+    public id: number = 0,
+    public url: string,
+    public description: string,
+    public author: string
+  ) {}
+
+  static fromJson(jsonResponse: string): Photo {
+    const jsonObject = JSON.parse(jsonResponse)[0];
+    return new Photo(
+      0,
+      jsonObject.urls.regular,
+      jsonObject.description,
+      jsonObject.user.name
+    );
+  }
+}
