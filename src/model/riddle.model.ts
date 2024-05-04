c3dd324a997beaaf16db3cf10563990b8926403b
karina (khavy) <164046438+dear-karina@users.noreply.github.com>
2024-05-04 05:52:50 +0000

    Added riddle model and unittest

--- src/model/riddle.model.ts
+++ src/model/riddle.model.ts
@@ -1,7 +1,19 @@
 // Eunho
+export class Riddle {
+  constructor(
+    public id: number = 0,
+    public title: string,
+    public question: string,
+    public answer: string
+  ) {}
 
-export type Riddle = {
-  title: string;
-  question: string;
-  answer: string;
-};
+  static fromJson(jsonResponse: string): Riddle {
+    const jsonObject = JSON.parse(jsonResponse)[0];
+    return new Riddle(
+      0,
+      jsonObject.title,
+      jsonObject.question,
+      jsonObject.answer
+    );
+  }
+}
