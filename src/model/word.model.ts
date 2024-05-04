9bc4f4d2bf124b5eca047f96369bf6ba17e6369d
sakurasakura <82525680+sakurasakura@users.noreply.github.com>
2024-05-04 06:28:20 +0000

    modified all models

--- src/model/word.model.ts
+++ src/model/word.model.ts
@@ -1,8 +1,34 @@
 // Bamby
-export type Word = {
-  id: number;
-  word: string;
-  form: "adj" | "v" | "n" | "adv";
-  meaning: string;
-  origin?: string;
-};
+export class Word {
+  constructor(
+    public id: number,
+    public word: string,
+    public form: "adj" | "v" | "n" | "adv",
+    public pronunciation: string,
+    public meaning: string,
+    public origin?: string
+  ) {}
+
+  static fromJson(json: string): Word {
+    const jsonObject = JSON.parse(json);
+    return new Word(
+      jsonObject.id,
+      jsonObject.word,
+      jsonObject.form,
+      jsonObject.pronunciation,
+      jsonObject.meaning,
+      jsonObject.origin
+    );
+  }
+  static fromObject(object: any): Word {
+    return new Word(
+      object.id,
+      object.word,
+      object.form,
+      object.pronunciation,
+      object.meaning,
+      object.origin
+    );
+  }
+}
+
