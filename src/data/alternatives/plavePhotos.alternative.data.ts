7cb238dd2bb50737de31c1d3e9265ae530aec8e0
karina (khavy) <164046438+dear-karina@users.noreply.github.com>
2024-05-05 03:49:01 +0000

    added photo alternative database

---
+++ src/data/alternatives/plavePhotos.alternative.data.ts
@@ -0,0 +1,33 @@
+const plavePhotos = [
+    {
+      id: 0,
+      url: "lib/alternative_images/plave_w4l.jpeg",
+      description: "Plave - Way 4 Luv",
+      author: ""
+    },
+    {
+      id: 1,
+      url: "lib/alternative_images/plave_t6s.jpeg",
+      description: "Plave - The 6th Summer",
+      author: ""
+    },
+    {
+      id: 2,
+      url: "lib/alternative_images/bamby_space.jpeg",
+      description: "Hamin's space",
+      author: ""
+    },
+    {
+      id: 3,
+      url: "lib/alternative_images/yejun_space.jpeg",
+      description: "Yejun's space",
+      author: ""
+    },
+    {
+      id: 4,
+      url: "lib/alternative_images/bamby_space.jpeg",
+      description: "Bamby's space",
+      author: ""
+    }
+  ];
+  
