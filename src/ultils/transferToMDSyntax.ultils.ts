009f9504b9f864308d7e1f85049e482bd22b8cc4
karina (khavy) <164046438+dear-karina@users.noreply.github.com>
2024-05-06 07:41:46 +0000

    added transfer word, riddle, photo

--- src/ultils/transferToMDSyntax.ultils.ts
+++ src/ultils/transferToMDSyntax.ultils.ts
@@ -1,5 +1,8 @@
 import { Fact } from "../models/fact.model"
+import { Photo } from "../models/photo.model"
 import { Quote } from "../models/quote.model"
+import { Riddle } from "../models/riddle.model"
+import { Word } from "../models/word.model"
 
 export const transfer_fact=(fact: Fact): string=>{
     return `>${fact.description}`
@@ -7,4 +10,13 @@ export const transfer_fact=(fact: Fact): string=>{
 
 export const transfer_quote=(quote: Quote): string=>{
     return `>${quote.quote}`
+}
+export const transfer_word=(word: Word): string=>{
+    return `> ${word.word}\n>\n> > (${word.form}) / ${word.pronunciation} /  \n> > ${word.meaning}.`
+}
+export const transfer_riddle=(riddle: Riddle): string=>{
+    return `>> ${riddle.question}\n\n<details close>\n\n<summary>🕺🏻 Reveal the answer here 🕺🏻</summary>\n\n${riddle.answer}\n</details>`
+}
+export const transfer_photo=(photo: Photo): string=>{
+    return `<img src="${photo.url}" width="1050px">\n<p align="center">${photo.description}</p>`
 }
