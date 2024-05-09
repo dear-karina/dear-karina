cae91b61ed52cc2fbe2c7b77e9a824b47100daa4
karina (khavy) <164046438+dear-karina@users.noreply.github.com>
2024-05-09 03:41:32 +0000

    fixed rewriting index module

--- src/data/currentIndex.ts
+++ src/data/currentIndex.ts
@@ -1,7 +1,7 @@
 export const currentIndexes={
-    fact_index:0,
-    photo_index:0,
-    quote_index:0,
-    riddle_index:0,
-    word_index:0
+  "fact_index": 2,
+  "photo_index": 0,
+  "quote_index": 0,
+  "riddle_index": 0,
+  "word_index": 0
 }
