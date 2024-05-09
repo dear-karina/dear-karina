2387d3014affd9d20f12b184b31592b2b3149288
sakurasakura <82525680+sakurasakura@users.noreply.github.com>
2024-05-09 03:32:08 +0000

    added current indexes

---
+++ src/data/constants/paramIndex.enum.ts
@@ -0,0 +1,9 @@
+export enum ParamIndex{
+    FACT,
+    PHOTO,
+    QUOTE,
+    RIDDLE,
+    WEATHER,
+    WORD
+}
+
---
+++ src/data/currentIndex.ts
@@ -0,0 +1,7 @@
+export const currentIndexes={
+    fact_index:0,
+    photo_index:0,
+    quote_index:0,
+    riddle_index:0,
+    word_index:0
+}
